48b06ccefe34ca775e6e52f8bd411b1550e2f167
joy <bob@qq.com>
2022-08-20 21:26:21 +0800

    push idea commit

--- src/main/java/com/school/git/GitTest.java
+++ src/main/java/com/school/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4 merge hot-fix");
         System.out.println("hello git5 merge hot-fix");
+        System.out.println("push test");
     }
 }
